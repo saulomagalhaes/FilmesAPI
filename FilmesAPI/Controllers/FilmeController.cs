91a905351959f069952dd22e4ec1ca346301f32f
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-07 21:51:30 -0300

    feat: cria o filmecontroller com o metodo post

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> _filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        _filmes.Add(filme);
+        HttpResponse response = HttpContext.Response;
+        response.StatusCode = 201;
+    }
+}
