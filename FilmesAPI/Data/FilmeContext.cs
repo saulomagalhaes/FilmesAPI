619087019b4f3bebd5c7184bfa032f1006531222
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-08 22:20:14 -0300

    feat: cria o contexto filme pro db

---
+++ FilmesAPI/Data/FilmeContext.cs
@@ -0,0 +1,14 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public DbSet<Filme> Filme { get; set; }
+        public FilmeContext(DbContextOptions<FilmeContext> opts ) : base(opts)
+        {
+
+        }
+    }
+}
