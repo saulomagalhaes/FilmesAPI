0a6c9961e45c86a33f503a00d9c69da0fca382b1
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-18 23:27:23 -0300

    feat(cinema): cria os dtos pra cinema

---
+++ FilmesAPI/Data/Dtos/Cinema/CreateCinemaDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos;
+
+public class CreateCinemaDto
+{
+    [Required(ErrorMessage = "O nome do cinema é obrigatório")]
+    public string Nome { get; set; }
+}
