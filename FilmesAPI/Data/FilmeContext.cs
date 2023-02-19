60583541e3399b9ad791f8b3cb3d7d1d6ec013ee
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-19 00:33:49 -0300

    feat(context): adiciona o cinema

--- FilmesAPI/Data/FilmeContext.cs
+++ FilmesAPI/Data/FilmeContext.cs
@@ -6,6 +6,7 @@ namespace FilmesAPI.Data;
 public class FilmeContext : DbContext
 {
     public DbSet<Filme> Filmes { get; set; }
+    public DbSet<Cinema> Cinemas { get; set; }
     public FilmeContext(DbContextOptions<FilmeContext> opts ) : base(opts)
     {
 
