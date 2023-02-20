3d1acd1afa1689b5c8275273b10ab9ed1a8b78cf
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-20 13:09:47 -0300

    feat(endereco): cria entidade endereco e seus dtos

---
+++ FilmesAPI/Data/Dtos/Enderecos/CreateEnderecoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FilmesAPI.Data.Dtos.Enderecos
+{
+    public class CreateEnderecoDto
+    {
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+    }
+}
---
+++ FilmesAPI/Models/Endereco.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Endereco
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O logradouro do endereço é obrigatório")]
+        public string Logradouro { get; set; }
+        [Required(ErrorMessage = "O numero do endereço é obrigatório")]
+        public int Numero { get; set; }
+    }
+}
