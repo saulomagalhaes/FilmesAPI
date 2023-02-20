679c69dfc364b7f491ab9c382dcf5267fe837a9f
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-20 13:10:30 -0300

    feat(endereco): adiciona profile automapper para endereco

---
+++ FilmesAPI/Profiles/EnderecoProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.Dtos.Enderecos;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Profiles
+{
+    public class EnderecoProfile : Profile
+    {
+        public EnderecoProfile()
+        {
+            CreateMap<CreateEnderecoDto, Endereco>();
+            CreateMap<UpdateEnderecoDto, Endereco>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<Endereco, ReadEnderecoDto>();
+        }
+    }
+}
+    
